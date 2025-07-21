c013d07a7b64cd6d927f85349dfe41832865bafd
Matthias Jugan <matthias.jugan@booleworks.com>
2025-07-21 22:20:23 +0200

    Extend SddWriter API

--- core/src/main/java/com/booleworks/logicng/io/writers/SddWriter.java
+++ core/src/main/java/com/booleworks/logicng/io/writers/SddWriter.java
@@ -12,6 +12,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -43,20 +44,47 @@ public class SddWriter {
      */
     public static void writeSdd(final File destination, final SddNode node, final Sdd sdd) throws IOException {
         try (
-                final BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(Files.newOutputStream(destination.toPath()), StandardCharsets.UTF_8))
+                final Writer writer = new OutputStreamWriter(Files.newOutputStream(destination.toPath()),
+                        StandardCharsets.UTF_8)
+        ) {
+            writeSdd(writer, node, sdd);
+        }
+    }
+
+    /**
+     * Exports the VTree and an SDD node of an SDD to text representation.
+     * <p>
+     * The first part will be the exported VTree using the format of
+     * {@link SddWriter#writeVTree(File, Sdd) writeVTree}.
+     * The second part is the exported SDD node following the following syntax:
+     * <ul>
+     *     <li>First line: {@code sdd {number of nodes} {id of root}}</li>
+     *     <li>{@code D {id of node} {number of sdd elements} [ {id of prime node} {id of sub node} ]+}</li>
+     *     <li>{@code L {id of leaf} {name of variable}}</li>
+     *     <li>{@code I {id of node} {id of left child} {id of right child}}"</li>
+     * </ul>
+     * <p>
+     * Remark: The ids of the nodes may be different to the ids used by LNG internally.
+     * @param writer the destination
+     * @param node   the node to export
+     * @param sdd    the sdd of the node
+     * @throws IOException if there was a problem writing the file
+     */
+    public static void writeSdd(final Writer writer, final SddNode node, final Sdd sdd) throws IOException {
+        try (
+                final BufferedWriter w = new BufferedWriter(writer)
         ) {
             final SddExportState state = new SddExportState();
-            writeVTree(sdd, state.vState, writer);
-            writer.newLine();
+            writeVTree(sdd, state.vState, w);
+            w.newLine();
             final int rootId = exportSdd(node, sdd, state);
-            writer.append(String.format("sdd %d %d", state.size, rootId));
-            writer.newLine();
+            w.append(String.format("sdd %d %d", state.size, rootId));
+            w.newLine();
             for (final String line : state.lines) {
-                writer.append(line);
-                writer.newLine();
+                w.append(line);
+                w.newLine();
             }
-            writer.flush();
+            w.flush();
         }
     }
 
@@ -118,12 +146,35 @@ public class SddWriter {
      * @throws IOException if there was a problem writing the file
      */
     public static void writeVTree(final File file, final Sdd sdd) throws IOException {
+        try (
+                final Writer writer =
+                        new OutputStreamWriter(Files.newOutputStream(file.toPath()), StandardCharsets.UTF_8)
+        ) {
+            writeVTree(writer, sdd);
+        }
+    }
+
+    /**
+     * Exports a VTree to text representation.
+     * <p>
+     * File syntax:
+     * <ul>
+     *     <li>First line: {@code vtree {number of nodes} {id of root}}</li>
+     *     <li>{@code L {id of leaf} {name of variable}}</li>
+     *     <li>{@code I {id of node} {id of left child} {id of right child}}</li>
+     * </ul>
+     * <p>
+     * Remark: The ids of the nodes may be different to the ids used by LNG internally.
+     * @param writer destination
+     * @param sdd    the sdd which vtree gets exported
+     * @throws IOException if there was a problem writing
+     */
+    public static void writeVTree(final Writer writer, final Sdd sdd) throws IOException {
         final VTreeExportState exportState = new VTreeExportState();
         try (
-                final BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(Files.newOutputStream(file.toPath()), StandardCharsets.UTF_8))
+                final BufferedWriter w = new BufferedWriter(writer)
         ) {
-            writeVTree(sdd, exportState, writer);
+            writeVTree(sdd, exportState, w);
         }
     }
 
